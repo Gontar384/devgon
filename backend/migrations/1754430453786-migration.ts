fabf485b2698e00ad2027bcdeb54557549642a65
Gontar384 <gontarek384@gmail.com>
2025-08-06 00:31:33 +0200

    Still configuring deploy

--- backend/migrations/1754430453786-migration.ts
+++ backend/migrations/1754430453786-migration.ts
@@ -1,14 +1,17 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class Migration1754430453786 implements MigrationInterface {
-    name = 'Migration1754430453786'
+  name = 'Migration1754430453786';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "product" DROP COLUMN "updatedAt"`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "product"
+      DROP COLUMN "updatedAt"`);
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "product" ADD "updatedAt" TIMESTAMP NOT NULL DEFAULT now()`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "product"
+        ADD "updatedAt" TIMESTAMP NOT NULL DEFAULT now()`,
+    );
+  }
 }
