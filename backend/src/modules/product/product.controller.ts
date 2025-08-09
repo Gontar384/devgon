58d41dcc4dade120728209b4fcafad362b7f67ec
Gontar384 <gontarek384@gmail.com>
2025-08-09 03:27:57 +0200

    Configured docker-compose to build and run locally

--- backend/src/modules/product/product.controller.ts
+++ backend/src/modules/product/product.controller.ts
@@ -2,7 +2,7 @@ import { Body, Controller, Get, Post } from '@nestjs/common';
 import { ProductService } from './product.service';
 import { CreateProductDto } from './dto/create-product.dto';
 
-@Controller('products')
+@Controller('api/products')
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
 
