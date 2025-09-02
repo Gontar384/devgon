93600b0f7c6f3b407b698fbf9d5a055aed3089ef
Gontar384 <gontarek384@gmail.com>
2025-09-02 02:16:48 +0200

    Styling homepage v2 fix v5

--- frontend/src/app/robots.txt/route.ts
+++ frontend/src/app/robots.txt/route.ts
@@ -1,15 +1,12 @@
 import { NextResponse } from 'next/server';
 
 export const GET = () => {
-  const isProd = process.env.NEXT_PUBLIC_ENV === 'prod';
   const siteName = process.env.NEXT_PUBLIC_DOMAIN;
 
   const content = `
 User-agent: *
 Allow: /
-
-${isProd ? `Sitemap: https://${siteName}/sitemap.xml` : ''}
-  `.trim();
+Sitemap: https://${siteName}/sitemap.xml`;
 
   return new NextResponse(content, {
     headers: { 'Content-Type': 'text/plain' },
