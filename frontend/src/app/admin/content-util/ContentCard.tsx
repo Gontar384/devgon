a2dff34f19fdbf66751749664895dbbd5b9c14c2
Gontar384 <gontarek384@gmail.com>
2026-02-17 20:34:33 +0100

    Modified content's order count label

--- frontend/src/app/admin/content-util/ContentCard.tsx
+++ frontend/src/app/admin/content-util/ContentCard.tsx
@@ -221,7 +221,7 @@ export function ContentCard({
           key={isEditing ? 'editing' : 'view'}
         >
           {!singleMode && (
-            <p className="absolute right-6 top-4 underline">
+            <p className="text-sm absolute right-3 top-3 px-1.5 underline border-2 rounded-full">
               {(content?.order ?? 0) + 1}
             </p>
           )}
