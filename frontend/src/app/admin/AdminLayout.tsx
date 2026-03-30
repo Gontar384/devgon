c9a5986b70aa9e0ad040ebe4aa3e4042c91f2348
Gontar384 <gontarek384@gmail.com>
2026-03-30 22:08:02 +0200

    Fix: fixed admin panel tabs bug

--- frontend/src/app/admin/AdminLayout.tsx
+++ frontend/src/app/admin/AdminLayout.tsx
@@ -2,36 +2,38 @@
 import React from 'react';
 import { Tabs, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import { usePathname } from 'next/navigation';
-import Link from 'next/link';
+import { NavLink } from '../layout/util/NavLink';
 
 export function AdminLayout({ children }: { children: React.ReactNode }) {
   const pathname = usePathname();
 
   return (
     <section className="w-full min-h-screen relative">
-      <Tabs value={pathname} className="mt-24">
-        <TabsList className="flex justify-center rounded-none bg-background gap-1">
-          <TabsTrigger
-            value="/admin/home"
-            className="cursor-pointer bg-background data-[state=active]:bg-primary data-[state=active]:text-primary-foreground hover:bg-accent hover:text-foreground active:bg-accent active:text-foreground"
-            id={undefined} //hydration mismatch error occurred
-            aria-controls={undefined}
-          >
-            <Link href="/admin/home" className="text-xl">
-              Strona główna
-            </Link>
-          </TabsTrigger>
-          <TabsTrigger
-            value="/admin/privacy-policy"
-            className="cursor-pointer bg-background data-[state=active]:bg-primary data-[state=active]:text-primary-foreground hover:bg-accent hover:text-foreground active:bg-accent active:text-foreground"
-            id={undefined} //hydration mismatch error occurred
-            aria-controls={undefined}
-          >
-            <Link href="/admin/privacy-policy" className="text-xl">
-              Polityka prywatności
-            </Link>
-          </TabsTrigger>
-        </TabsList>
+      <Tabs value={pathname} className="mt-20">
+        <div className="overflow-x-auto py-3">
+          <TabsList className="flex justify-center gap-2 min-w-max px-2 md:px-4 py-6 bg-background rounded-none">
+            <TabsTrigger
+              value="/admin/home"
+              className="cursor-pointer bg-background border data-[state=active]:bg-primary data-[state=active]:text-primary-foreground hover:bg-accent hover:text-foreground active:bg-accent active:text-foreground"
+              id={undefined}
+              aria-controls={undefined}
+            >
+              <NavLink href="/admin/home" className="text-xl">
+                Strona główna
+              </NavLink>
+            </TabsTrigger>
+            <TabsTrigger
+              value="/admin/privacy-policy"
+              className="cursor-pointer bg-background border data-[state=active]:bg-primary data-[state=active]:text-primary-foreground hover:bg-accent hover:text-foreground active:bg-accent active:text-foreground"
+              id={undefined}
+              aria-controls={undefined}
+            >
+              <NavLink href="/admin/privacy-policy" className="text-xl">
+                Polityka prywatności
+              </NavLink>
+            </TabsTrigger>
+          </TabsList>
+        </div>
       </Tabs>
       <div className="mt-8">{children}</div>
     </section>
