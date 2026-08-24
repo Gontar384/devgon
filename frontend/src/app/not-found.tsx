e2e0a2e8effad77e01d877661aba5a761d081698
Gontar384 <gontarek384@gmail.com>
2026-08-24 20:56:42 +0200

    Translation: not-found + privacy-policy

--- frontend/src/app/not-found.tsx
+++ frontend/src/app/not-found.tsx
@@ -4,7 +4,7 @@ export default function NotFoundPage() {
   return (
     <section className="flex flex-col justify-center items-center h-screen -mt-16">
       <h1 className="text-7xl font-bold">404</h1>
-      <p className="my-4 text-xl">Strona nie została znaleziona</p>
+      <p className="my-4 text-xl">Page not found</p>
       <Frown className="!w-10 !h-10" />
     </section>
   );
